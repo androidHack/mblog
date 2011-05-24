e84c55d9eaebc07630b84ed55ea6ff89fd48b7e7
admin <admin@localhost.localdomain>
2011-05-24 09:36:21 +0800

    初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mblog::Application.config.secret_token = 'c8428a4d0cfa3c1ba54772c3a30b5136aa87887a1c985175a101d661b3d134295c8aa95b67d8724b61fdcd8479f5769af41c416589fa6bd9df1c3d3a7cba8fce'
